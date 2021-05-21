545fccd0293ef4b047c42740ccbd213652419ad4
GorkaTellechea <gorkeles@gmail.com>
2021-05-21 16:21:47 +0200

    Por fin va la puta base de datos

--- tienda.php
+++ tienda.php
@@ -8,14 +8,14 @@ $yaBuscado = false;
 $arrayProducts = array();
 
 // Comprobamos que el formulario haya sido enviado con las variables que hayamos puesto en index.view, deben llamarse igual!
-if (isset($_POST['rutas'])) {
+if (isset($_POST['introducir'])) {
     $conexion = new mysqli('localhost', 'root', '', 'tfg');
     //conectamos mi base de datos 'tfg'
 
-    $provincia = $_POST['provincia'];
-    $nivel = $_POST['nivel'];
-    $distancia = $_POST['distancia'];
-    $descripcion = $_POST['descripcion'];
+    $id_product = $_POST['id_product'];
+    $title = $_POST['title'];
+    $price = $_POST['price'];
+    $img_url = $_POST['img_url'];
 
 
     $conexion = new mysqli('localhost', 'root', '', 'tfg');
@@ -26,22 +26,22 @@ if (isset($_POST['rutas'])) {
         die('Lo siento, hubo un problema con el servidor');
     } else {
 
-        // Antes de insertar, hacemos una query para ver si existe email
-        $existeProductoSql = 'SELECT * from rutas where descripcion=\'' . $descripcion . '\'';
+        // Antes de insertar, hacemos una query para ver si existe producto
+        $existeProductoSql = 'SELECT * from products where id_product=\'' . $id_product . '\'';
 
 
         $conexion->query($existeProductoSql);
         if ($conexion->affected_rows >= 1) {
             echo "<script type='text/javascript'>
-		alert('Ya has introducido esa ruta');
-		window.location.href='rutas.view.php';
+		alert('Ya has introducido ese producto');
+		window.location.href='tienda.view.php';
 		</script>";
             return;
         }
 
 
 
-        $sql = "INSERT INTO rutas (id_ruta,provincia,nivel,distancia,descripcion) VALUES (null,'$provincia','$nivel','$distancia','$descripcion')";
+        $sql = "INSERT INTO products (id_product,title,price,img_url) VALUES ('$id_product','$title','$price','$img_url')";
 
         $conexion->query($sql);
         //Hacemos un query a la base de datos
@@ -49,8 +49,8 @@ if (isset($_POST['rutas'])) {
         if ($conexion->affected_rows >= 1) {
             //si todo va bien saluda al usuario
             echo "<script type='text/javascript'>
-        			alert('Has introducido $descripcion');
-        			window.location.href='rutas.view.php';
+        			alert('Has introducido $title');
+        			window.location.href='tienda.view.php';
         			</script>";
 
             hayConexion();
@@ -58,43 +58,34 @@ if (isset($_POST['rutas'])) {
             //sino le dice que lo intente de nuevo
             echo "<script type='text/javascript'>
         			alert('Intentalo de nuevo, por favor');
-        			window.location.href='rutas.view.php';
+        			window.location.href='tienda.view.php';
         			</script>";
         }
     }
 }
 
 
-if (isset($_POST['buscarRutas'])) {
+if (isset($_POST['buscarProductos'])) {
     $conexion = new mysqli('localhost', 'root', '', 'tfg');
     //conectamos mi base de datos 'tfg'
 
     $yaBuscado = true;
-    $busqueda = $_POST['provincia'];
-    $tipoBusquedaRuta = $_POST['tipoBusquedaRuta'];
+    $busqueda = $_POST['id_product'];
+    $tipoBusquedaProducto = $_POST['tipoBusquedaProducto'];
 
 
-    if ($tipoBusquedaRuta == 'PROVINCIA') {
+    if ($tipoBusquedaProducto == 'id_product') {
 
-        $buscarProductosSql = "SELECT * from rutas where provincia LIKE '%$busqueda%'";
-    } elseif ($tipoBusquedaRuta == 'NIVEL') {
+        $buscarProductosSql = "SELECT * from products where id_product LIKE '%$busqueda%'";
+    } elseif ($tipoBusquedaProducto == 'title') {
 
-        $buscarProductosSql = "SELECT * from rutas where nivel LIKE '%$busqueda%'";
-    } elseif ($tipoBusquedaRuta == 'DISTANCIA') {
+        $buscarProductosSql = "SELECT * from products where title LIKE '%$busqueda%'";
+    } elseif ($tipoBusquedaProducto == 'price') {
 
-        $buscarProductosSql = "SELECT * from rutas where distancia LIKE '$busqueda'";
-    } elseif ($tipoBusquedaRuta == 'DESCRIPCION') {
+        $buscarProductosSql = "SELECT * from products where price LIKE '$busqueda'";
+    } elseif ($tipoBusquedaProducto == 'img_url') {
 
-        $buscarProductosSql = "SELECT * from rutas where descripcion LIKE '%$busqueda%'";
-    } else {
-
-        if ($busqueda < 300) {
-            $buscarProductosSql = "SELECT * from rutas where nivel LIKE '%fácil%'";
-        } elseif ($busqueda >= 300 && $busqueda < 600) {
-            $buscarProductosSql = "SELECT * from rutas where nivel LIKE '%media%'";
-        } elseif ($busqueda > 600) {
-            $buscarProductosSql = "SELECT * from rutas where nivel LIKE '%dificil%'";
-        }
+        $buscarProductosSql = "SELECT * from products where img_url LIKE '%$busqueda%'";
     }
 
     $resultado = $conexion->query($buscarProductosSql);
