25f6b97e4a71aba8aaf8664aae007e27021fc3c3
Olga Cobo Lopez <olgacobolopez@gmail.com>
2021-05-24 11:10:09 +0200

    cambio view pedido realizado y codico de envio mail pedido

--- tienda.php
+++ tienda.php
@@ -1,99 +1,36 @@
 <?php
 
 include 'funciones.php';
-$errores = '';
-$enviado = true;
 
-$yaBuscado = false;
-$arrayProducts = array();
+if(isset($_POST['email'])) {
 
-// Comprobamos que el formulario haya sido enviado con las variables que hayamos puesto en index.view, deben llamarse igual!
-if (isset($_POST['introducir'])) {
-    $conexion = new mysqli('localhost', 'root', '', 'tfg');
-    //conectamos mi base de datos 'tfg'
+    // correo de la dueña al que se enviaran los emails y su asunto
+    $email_to = "olga.cobo@ezzing.com";
+    $email_subject = "Pedido";
 
-    $id_product = $_POST['id_product'];
-    $title = $_POST['title'];
-    $price = $_POST['price'];
-    $img_url = $_POST['img_url'];
-
-
-    $conexion = new mysqli('localhost', 'root', '', 'tfg');
-    //conectamos mi base de datos 'tfg'
-
-
-    if ($conexion->connect_errno) {
-        die('Lo siento, hubo un problema con el servidor');
-    } else {
-
-        // Antes de insertar, hacemos una query para ver si existe producto
-        $existeProductoSql = 'SELECT * from products where id_product=\'' . $id_product . '\'';
-
-
-        $conexion->query($existeProductoSql);
-        if ($conexion->affected_rows >= 1) {
-            echo "<script type='text/javascript'>
-		alert('Ya has introducido ese producto');
-		window.location.href='tienda.view.php';
-		</script>";
-            return;
-        }
-
-
-
-        $sql = "INSERT INTO products (id_product,title,price,img_url) VALUES ('$id_product','$title','$price','$img_url')";
-
-        $conexion->query($sql);
-        //Hacemos un query a la base de datos
-
-        if ($conexion->affected_rows >= 1) {
-            //si todo va bien saluda al usuario
-            echo "<script type='text/javascript'>
-        			alert('Has introducido $title');
-        			window.location.href='tienda.view.php';
-        			</script>";
-
-            hayConexion();
-        } else {
-            //sino le dice que lo intente de nuevo
-            echo "<script type='text/javascript'>
-        			alert('Intentalo de nuevo, por favor');
-        			window.location.href='tienda.view.php';
-        			</script>";
-        }
-    }
-}
-
-
-if (isset($_POST['buscarProductos'])) {
-    $conexion = new mysqli('localhost', 'root', '', 'tfg');
-    //conectamos mi base de datos 'tfg'
-
-    $yaBuscado = true;
-    $busqueda = $_POST['id_product'];
-    $tipoBusquedaProducto = $_POST['tipoBusquedaProducto'];
-
-
-    if ($tipoBusquedaProducto == 'id_product') {
-
-        $buscarProductosSql = "SELECT * from products where id_product LIKE '%$busqueda%'";
-    } elseif ($tipoBusquedaProducto == 'title') {
-
-        $buscarProductosSql = "SELECT * from products where title LIKE '%$busqueda%'";
-    } elseif ($tipoBusquedaProducto == 'price') {
-
-        $buscarProductosSql = "SELECT * from products where price LIKE '$busqueda'";
-    } elseif ($tipoBusquedaProducto == 'img_url') {
-
-        $buscarProductosSql = "SELECT * from products where img_url LIKE '%$busqueda%'";
+    // validacion de los datos recogidos por el formulario
+    if(!isset($_POST['nombre']) ||
+    !isset($_POST['phone']) ||
+    !isset($_POST['email']) ||
+    !isset($_POST['direction'])) {
+        echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
+        die();
     }
 
-    $resultado = $conexion->query($buscarProductosSql);
+    $email_message = "Detalles del pedido:\n\n";
+    $email_message .= "Nombre: " . $_POST['nombre'] . "\n";
+    $email_message .= "E-mail: " . $_POST['email'] . "\n";
+    $email_message .= "Telefono: " . $_POST['phone'] . "\n";
+    $email_message .= "Direccion: " . $_POST['direction'] . "\n\n";
 
-    while ($row = $resultado->fetch_assoc()) {
+    
 
-        array_push($arrayProducts, $row);
-    }
+    if(mail($email_to, $email_subject, $email_message)){
+        include 'pedidoEnviado.view.php';
+        die();
+    }else{
+        echo "Por favor, vuelva atrás y verifique la información ingresada";
+    };
 }
 
 
